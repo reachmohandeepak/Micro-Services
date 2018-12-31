7d873e2abe5a94fd75f5b44e1c6a1605873a0e17
reachdeepakgowda@gmail.com <reachdeepakgowda@gmail.com>
2018-12-31 18:58:16 +0530

    Initial microservice commit

---
+++ netflix-zuul-api-gateway-server/src/main/java/com/my/microservice/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplication.java
@@ -0,0 +1,14 @@
+package com.my.microservice.netflixzuulapigatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixZuulApiGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixZuulApiGatewayServerApplication.class, args);
+	}
+
+}
+
